f8b8f3ebba6a2271f8044efdc07a2f5a0f8542fb
Maria <naplesaries2014@gmail.com>
2022-10-18 20:50:07 +0200

    form

--- react-app/src/components/Filters.js
+++ react-app/src/components/Filters.js
@@ -2,20 +2,36 @@ import React from 'react'
 import "../style/filters.css";
 export default function Filters() {
   return (
-    <form className="filters">
-      <select className="lan">
+    <form className="form">
+      <select className="form-input">
+        <option value="" disabled selected hidden>
+          Language
+        </option>
         <option value="french">French</option>
         <option value="dutch">Dutch</option>
         <option value="english">English</option>
-        <option value="german">German</option>
+        <option value="german">German</option>Language
       </select>
-      <input type="text" placeholder="Children Ages (e.g 2,8)"></input>
-      <input type="number" placeholder="Postcode" min="1000" max="1200"></input>
-      <select id="funding">
+      <select className="form-input">
+        <option value="" disabled selected hidden>
+          Funding type
+        </option>
         <option value="public">Public</option>
-        <option value="private">Private</option>
+        <option value="private">Private</option>Type
       </select>
-      <button>Find school</button>
+      <input
+        type="number"
+        className="form-input"
+        placeholder="Postcode"
+        min="1000"
+        max="1200"
+      ></input>
+      <input
+        className="form-input"
+        type="text"
+        placeholder="Children Ages (e.g 2,8)"
+      ></input>
+      <button id="submit_btn">Find school</button>
     </form>
   );
 }
