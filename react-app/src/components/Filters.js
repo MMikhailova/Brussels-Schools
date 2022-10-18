31244a4a451325a0502595403e20b3116ac30129
Maria <naplesaries2014@gmail.com>
2022-10-18 11:07:07 +0200

    intro

--- react-app/src/components/Filters.js
+++ react-app/src/components/Filters.js
@@ -1,9 +1,21 @@
 import React from 'react'
-
+import "../style/filters.css";
 export default function Filters() {
   return (
-    <div>
-      
-    </div>
-  )
+    <form className="filters">
+      <select className="lan">
+        <option value="french">French</option>
+        <option value="dutch">Dutch</option>
+        <option value="english">English</option>
+        <option value="german">German</option>
+      </select>
+      <input type="text" placeholder="Children Ages (e.g 2,8)"></input>
+      <input type="number" placeholder="Postcode" min="1000" max="1200"></input>
+      <select id="funding">
+        <option value="public">Public</option>
+        <option value="private">Private</option>
+      </select>
+      <button>Find school</button>
+    </form>
+  );
 }
--- react-app/src/components/Intro.js
+++ react-app/src/components/Intro.js
@@ -1,28 +1,14 @@
 import React from 'react'
-
+import "../style/introduction.css";
 export default function Intro() {
   return (
-      <article>
-        Auto Sidebar This sidebar is as tall as its content (the links), and is
-        always shown. Scroll down the page to see the result. Some text to
-        enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no
-        quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui
-        causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus
-        repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae
-        voluptatibus. Some text to enable scrolling.. Lorem ipsum dolor sit
-        amet, illum definitiones no quo, maluisset concludaturque et eum, altera
-        fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur
-        eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad.
-        Eum no molestiae voluptatibus. Some text to enable scrolling.. Lorem
-        ipsum dolor sit amet, illum definitiones no quo, maluisset
-        concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius
-        te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et.
-        Inciderint efficiantur his ad. Eum no molestiae voluptatibus. Some text
-        to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no
-        quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui
-        causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus
-        repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae
-        voluptatibus.
-      </article>
+    <div className="introduction">
+      <h3>Find the BEST school in Brussels for your child</h3>
+      <p>
+        All the key information about all public and private schools in Brussels,
+        Belgium: Languages, class sizes,fees, school bus availability, etc.<br/> Enter your preferences and find the
+        best school that best fits your child's needs.
+      </p>
+    </div>
   );
 }
